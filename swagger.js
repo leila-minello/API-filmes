ea400221d24d5b1a2acfaceead943baefbdc402b
leila <132851093+leila-minello@users.noreply.github.com>
2024-09-05 23:55:48 -0300

    configuração + instalação swagger autogen

--- swagger.js
+++ swagger.js
@@ -1,1 +1,27 @@
 const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'API de Log de Filmes - CartaCaixa',
+        description:'Documentação da API usando Swagger Autogen'
+    },
+
+    host: 'localhost:3000',
+    schemes: ['http'],
+    securityDefinitions: {
+        bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+        },
+    },
+
+    security: [{ bearerAuth: [] }],
+};
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./app.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    require('./app');
+});
