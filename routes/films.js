7c6155a215ad47e296cd5c8bd7ab29544d17e145
leila <132851093+leila-minello@users.noreply.github.com>
2024-09-06 13:52:06 -0300

    documentação adicionada em todas as rotas (films.js)

--- routes/films.js
+++ routes/films.js
@@ -27,7 +27,24 @@ let validaFilme = (req, res, next) => {
     next();
 };
 
+//middleware para procurar filme por id
+let getFilm = async (req, res, next) => {
+    let id = req.params.id;
+    try {
+        let film = await FilmModel.getFilmById(id);
+        if (!film) {
+            return res.status(404).json({ status: false, error: "Filme não encontrado!" });
+        }
+        req.film = film;
+        next();
+    } catch (error) {
+        return res.status(500).json({ status: false, error: error.message });
+    }
+};
+
 router.use(verificarToken);
+
+//ROTAS FILMS.JS
 
 /**
  * @swagger
@@ -109,12 +126,12 @@ router.get("/", async (req, res) => {
  * @swagger
  * /api/films/melhores:
  *   get:
- *     summary: Lista os melhores filmes
- *     description: Retorna uma lista dos filmes que receberam nota 5.
+ *     summary: lista os melhores filmes (nota 5)
+ *     description: retorna uma lista dos filmes que receberam nota 5
  *     tags: [Filmes]
  *     responses:
  *       200:
- *         description: Lista de filmes com nota 5 retornada com sucesso.
+ *         description: sucesso ao retornar a lista de melhores filmes
  *         content:
  *           application/json:
  *             schema:
@@ -133,15 +150,15 @@ router.get("/", async (req, res) => {
  *                         example: "64e5ac7d5432d9bf0348afdc"
  *                       movie:
  *                         type: string
- *                         example: "Inception"
+ *                         example: "The Wall"
  *                       director:
  *                         type: string
- *                         example: "Christopher Nolan"
+ *                         example: "Alan Parker"
  *                       nota:
  *                         type: integer
  *                         example: 5
  *       500:
- *         description: Erro ao listar os melhores filmes.
+ *         description: erro ao listar os melhores filmes
  *         content:
  *           application/json:
  *             schema:
@@ -152,7 +169,7 @@ router.get("/", async (req, res) => {
  *                   example: false
  *                 error:
  *                   type: string
- *                   example: "Erro ao listar os melhores filmes."
+ *                   example: "erro ao listar os melhores filmes!"
  */
 router.get("/melhores", async (req, res) => {
     try {
@@ -163,39 +180,24 @@ router.get("/melhores", async (req, res) => {
     }
 });
 
-//middleware para procurar filme por id
-let getFilm = async (req, res, next) => {
-    let id = req.params.id;
-    try {
-        let film = await FilmModel.getFilmById(id);
-        if (!film) {
-            return res.status(404).json({ status: false, error: "Filme não encontrado!" });
-        }
-        req.film = film;
-        next();
-    } catch (error) {
-        return res.status(500).json({ status: false, error: error.message });
-    }
-};
-
-/** 
+/**
  * @swagger
- * /api/films/{id}
- *     get:
- *      summary: obter filme pelo seu ID
- *      description: retorna os detalhes de um filme específico obtido pelo seu ID
- *      tags: [Filmes]
- *      parameters: 
- *         - in: path
+ * /films/{id}:
+ *   get:
+ *     summary: obtem um filme pelo ID
+ *     description: retorna os detalhes de um filme específico com base em seu ID
+ *     tags:
+ *       - Filmes
+ *     parameters:
+ *       - in: path
  *         name: id
  *         required: true
- *         description: o ID do filme a ser pesquisado
+ *         description: ID do filme que você deseja recuperar
  *         schema:
  *           type: string
- *           example: "66da00277075d81206b12fa9"
- *      responses:
+ *     responses:
  *       200:
- *         description: filme encontrado com sucesso
+ *         description: filme recuperado com sucesso!
  *         content:
  *           application/json:
  *             schema:
@@ -203,24 +205,27 @@ let getFilm = async (req, res, next) => {
  *               properties:
  *                 status:
  *                   type: boolean
- *                   example: true
+ *                   description: status da operação
  *                 film:
  *                   type: object
  *                   properties:
  *                     id:
  *                       type: string
- *                       example: "66da00277075d81206b12fa9"
+ *                       description: ID do filme
  *                     movie:
  *                       type: string
- *                       example: "The Wall"
+ *                       description: título do filme
+ *                       example: "Click"
  *                     director:
  *                       type: string
- *                       example: "Alan Parker"
+ *                       description: diretor do filme
+ *                       example: "Frank Coraci"
  *                     nota:
- *                       type: integer
- *                       example: 5
+ *                       type: number
+ *                       description: nota do filme
+ *                       example: 3
  *       404:
- *         description: filme não foi encontrado
+ *         description: filme não encontrado
  *         content:
  *           application/json:
  *             schema:
@@ -228,18 +233,102 @@ let getFilm = async (req, res, next) => {
  *               properties:
  *                 status:
  *                   type: boolean
+ *                   description: status da operação
  *                   example: false
  *                 error:
  *                   type: string
- *                   example: "filme não encontrado!"
- * 
-*/
+ *                   description: mensagem de erro
+ *                   example: filme não encontrado
+ *       500:
+ *         description: erro interno do servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: status da operação
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   description: mensagem de erro
+ *                   example: erro no servidor
+ */
 router.get("/:id", getFilm, (req, res) => {
     res.json({ status: true, film: req.film });
 });
 
-
-//rota para criar um novo filme (somente para admins)
+/**
+ * @swagger
+ * /films:
+ *   post:
+ *     summary: cria um novo filme
+ *     description: permite a criação de um novo registro de filme (apenas admins)
+ *     tags:
+ *       - Filmes
+ *     security:
+ *       - bearerAuth: [] # indica que a autenticação JWT é necessária
+ *     requestBody:
+ *       description: dados do novo registro que será criado
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - movie
+ *               - director
+ *               - nota
+ *             properties:
+ *               movie:
+ *                 type: string
+ *                 description: título do filme
+ *                 example: "Stop Making Sense" 
+ *               director:
+ *                 type: string
+ *                 description: diretor do filme
+ *                 example: "Jonathan Demme"
+ *               nota:
+ *                 type: number
+ *                 description: nota do filme (máximo 5, mínimo 1)
+ *                 example: 5
+ *     responses:
+ *       200:
+ *         description: filme criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: status da operação
+ *                 film:
+ *                   type: object
+ *                   properties:
+ *                     movie:
+ *                       type: string
+ *                       description: título do filme
+ *                     director:
+ *                       type: string
+ *                       description: diretor do filme
+ *                     nota:
+ *                       type: number
+ *                       description: nota do filme
+ *       500:
+ *         description: erro interno do servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: status da operação
+ *                 error:
+ *                   type: string
+ *                   description: mensagem de erro
+ */
 router.post("/", verificaAdmin, validaFilme, async (req, res) => {
     try {
         const novoFilme = await FilmModel.novoFilme(req.movie, req.director, req.nota);
@@ -249,7 +338,105 @@ router.post("/", verificaAdmin, validaFilme, async (req, res) => {
     }
 });
 
-//rota para atualizar dados de um filme já existente
+/**
+ * @swagger
+ * /films/{id}:
+ *   put:
+ *     summary: aualiza dados de um filme existente
+ *     description: atualiza os dados de um filme existente (somente admins)
+ *     tags:
+ *       - Filmes
+ *     security:
+ *       - bearerAuth: [] # Indica que a autenticação JWT é necessária
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do filme que você deseja atualizar
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: dados atualizados do filme
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - movie
+ *               - director
+ *               - nota
+ *             properties:
+ *               movie:
+ *                 type: string
+ *                 description: nome atualizado
+ *                 example: "The Wall - *Pink Floyd*"
+ *               director:
+ *                 type: string
+ *                 description: diretor atualizado
+ *                 example: "Alan Parker"
+ *               nota:
+ *                 type: number
+ *                 description: nota atualizada
+ *                 example: 5
+ *     responses:
+ *       200:
+ *         description: filme atualizado com sucesso!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: status da operação
+ *                 film:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: ID do filme
+ *                     movie:
+ *                       type: string
+ *                       description: novo título do filme
+ *                       example: "The Wall - Pink Floyd"
+ *                     director:
+ *                       type: string
+ *                       description: novo diretor
+ *                       example: "Alan Parker"
+ *                     nota:
+ *                       type: number
+ *                       description: nota atualizada
+ *                       example: 5
+ *       404:
+ *         description: filme não encontrado!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: status da operação
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   description: mensagem de erro
+ *                   example: filme não encontrado!
+ *       500:
+ *         description: erro interno do servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: status da operação
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   description: erro no servidor!
+ */
 router.put("/:id", verificaAdmin, validaFilme, getFilm, async (req, res) => {
     try {
         const filmeAtualizado = await FilmModel.attFilme(req.film.id, req.movie, req.director, req.nota);
@@ -259,7 +446,80 @@ router.put("/:id", verificaAdmin, validaFilme, getFilm, async (req, res) => {
     }
 });
 
-//rota para deletar um filme pelo seu ID
+/**
+ * @swagger
+ * /films/{id}:
+ *   delete:
+ *     summary: deleta um filme pelo seu ID
+ *     description: deleta um filme existente do banco de dados com base no seu ID (somente admins)
+ *     tags:
+ *       - Filmes
+ *     security:
+ *       - bearerAuth: [] # Indica que a autenticação JWT é necessária
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do filme que você deseja deletar
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: filme deletado com sucesso!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: status da operação
+ *                 oldFilm:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: ID do filme deletado
+ *                     movie:
+ *                       type: string
+ *                       description: Título do filme deletado
+ *                     director:
+ *                       type: string
+ *                       description: Diretor do filme deletado
+ *                     nota:
+ *                       type: number
+ *                       description: Nota do filme deletado
+ *       404:
+ *         description: filme não encontrado!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: status da operação
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   description: mensagem de erro
+ *                   example: filme não encontrado!
+ *       500:
+ *         description: erro interno do servidor!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   description: status da operação
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   description: mensagem de erro
+ *                   example: erro de servidor!
+ */
 router.delete("/:id", verificaAdmin, getFilm, async (req, res) => {
     try {
         await FilmModel.deletaFilme(req.params.id);
